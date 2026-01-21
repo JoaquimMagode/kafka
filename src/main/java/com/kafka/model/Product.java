f712982728a9a1f2b45518c206ff06e869c644b1
Joaquim Magode <135075390+JoaquimMagode@users.noreply.github.com>
2026-01-21 14:31:49 +0530

    Update

---
+++ src/main/java/com/kafka/model/Product.java
@@ -0,0 +1,7 @@
+package com.kafka.model;
+
+public class Product {
+    private String id;
+    private String name;
+    private double price; 
+}
--- src/main/java/com/kafka/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.kafka.service;
-
-public class UserService {
-
-    List<User> users = new ArrayList<User>();
-
-    
-}
